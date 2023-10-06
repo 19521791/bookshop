3da61ba951e8efb90f1fb0ac1a30ba5c0d40909f
Nguyen Phi Long <nguyenlong@pop-os.localdomain>
2023-10-06 11:42:42 +0700

    fix: small fix in update book

--- app/commands/v1/books/update.rb
+++ app/commands/v1/books/update.rb
@@ -1,62 +1,67 @@
 class V1::Books::Update
-    prepend SimpleCommand
-    attr_reader :params
-  
-    def initialize(params)
-      @params = params
-    end
-  
-    def call
-      book = Book.includes(:categories).find_by(id: book_id)
+  prepend SimpleCommand
+  attr_reader :params
 
-      return errors.add(:book, 'not found') unless book
-
-      return book.errors unless book.update(book_params)
-      
-      update_book_categories(book, book_params[:book_categories_attributes])
-
-      BookPresenter.new(book).json_response
-    end
-
-    private 
-
-    def update_book_categories(book, book_categories_params)
-      existing_category_ids = book.book_categories.pluck(:category_id)
-    
-      new_category_ids = book_categories_params.map { |bc| bc['category_id'].to_i }
-    
-      if existing_category_ids.sort == new_category_ids.sort
-        return 
-      end
-    
-      to_remove = existing_category_ids - new_category_ids
-      to_add = new_category_ids - existing_category_ids
-    
-      book.book_categories.where(category_id: to_remove).destroy_all
-    
-      to_add.each do |category_id|
-        book.book_categories.create(category_id: category_id)
-      end
-    end
-    
-    
-    def book_id
-      params[:id]
-    end
-
-
-    def book_params
-      params.permit(
-        :title,
-        :author,
-        :description,
-        :thumbnail,
-        :rating,
-        :price,
-        :stock,
-        book_categories_attributes: [:id, :category_id, :_allow_destroy]
-      )
-    end
-      
+  def initialize(params)
+    @params = params
   end
-  
+
+  def call
+    book = Book.includes(:categories).find_by(id: book_id)
+
+    return errors.add(:book, 'not found') unless book
+
+    update_book_categories(book, book_params[:book_categories_attributes])
+
+    # return book.errors unless book.update(book_params)
+
+    BookPresenter.new(book).json_response
+  end
+
+  private
+
+  def update_book_categories(book, book_categories_params)
+    return nil unless book_categories_params.present?
+
+    category_ids = Set.new
+    unique_book_categories_params = []
+
+    book_categories_params.each do |bc|
+      category_id = bc[:category_id]
+
+      if category_ids.include?(category_id)
+
+        errors.add(:base, "Duplicate category_id #{category_id}")
+      else
+        category = Category.find_by(id: category_id)
+        if category
+          category_ids.add(category_id)
+          unique_book_categories_params << bc
+        else
+          errors.add(:base, "Category with ID #{category_id} not found.")
+        end
+      end
+    end
+
+    book.book_categories.destroy_all
+    book.book_categories.create(unique_book_categories_params)
+  end
+
+  def book_id
+    params[:id]
+  end
+
+  def book_params
+    params.permit(
+      :title,
+      :author,
+      :description,
+      :thumbnail,
+      :rating,
+      :price,
+      :stock,
+      book_categories_attributes: [:id, :category_id, :_allow_destroy]
+    )
+  end
+
+end
