f619e201ae272cbba675170ecdde643d6b48a9e9
douglus <douglus@rexy.tech>
2025-03-19 13:03:33 +0700

    fix: remove cron job

--- app/commands/utils.rb
+++ app/commands/utils.rb
@@ -9,4 +9,23 @@ module Utils
     ::Rails.logger.info("#{green}Logger debugging: #{string}#{reset}")
     ::Rails.logger.info("\n")
   end
+
+  def parse_expired_time(signed_url)
+    query_params = URI.parse(signed_url).query
+    return nil unless query_params
+  
+    params_hash = URI.decode_www_form(query_params).to_h
+    
+    amz_date = params_hash['X-Amz-Date']
+    expires_in = params_hash['X-Amz-Expires']
+  
+    return nil unless amz_date && expires_in
+  
+    created_at = Time.strptime(amz_date, '%Y%m%dT%H%M%SZ')
+    expired_at = created_at + expires_in.to_i
+  
+    expired_at
+  end
+  
+  
 end
--- app/jobs/auto_gen_signed_url_s3.rb
+++ app/jobs/auto_gen_signed_url_s3.rb
@@ -5,7 +5,7 @@ class AutoGenSignedUrlS3 < ApplicationJob
     attachments&.each do |attachment|
       presigned_url = aws_service.generate_presigned_url(attachment.file_name)
 
-      expired_at_vn = parse_expired_time(presigned_url)
+      expired_at_vn = ::Utils.parse_expired_time(presigned_url)
 
       attachment.update(signed_url: presigned_url, expired_at: expired_at_vn)
 
@@ -18,26 +18,11 @@ class AutoGenSignedUrlS3 < ApplicationJob
 
   private
 
-  def check_expired_time(expired_at)
-    expired_at.to_date == ::Date.tomorrow
-  end
-
-  def parse_expired_time(signed_url)
-    query_params = ::URI.parse(signed_url).query
-    params_hash = ::URI.decode_www_form(query_params).to_h
-
-    amz_date = params_hash['X-Amz-Date']
-    created_at = ::Time.strptime(amz_date, '%Y%m%dT%H%M%SZ').in_time_zone('UTC')
-
-    expired_at = params_hash['X-Amz-Expires'].to_i
-    (created_at + expired_at.seconds)
-  end
-
   def aws_service
     @aws_service ||= ::AwsService.new
   end
 
   def attachments
-    ::Attachment.where("expired_at BETWEEN ? AND ?", ::Time.current, 5.hours.from_now)
+    ::Attachment.where("expired_at BETWEEN ? AND ?", ::Time.current, 12.hours.from_now)
   end
 end
