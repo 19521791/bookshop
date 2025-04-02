5d5b024ed104b78e26c867d1ecef9ec6d2b5bab0
douglus <douglus@rexy.tech>
2025-04-02 21:19:11 +0700

    fix: fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,20 @@
+allowed_origins = [
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',              
+  'https://172.96.190.154',           
+  'https://douglusnguyen.site'                       
+].compact
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins do |source, _|
+      allowed_origins.any? { |origin| 
+        origin.is_a?(Regexp) ? origin.match?(source) : origin == source
+      }
+    end
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
