d4cc751f59d5f2047bcedb48f89c6079640a6465
douglus <douglus@rexy.tech>
2025-04-02 15:56:09 +0700

    fix: fix binding puma

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@ workers ENV.fetch("WEB_CONCURRENCY") {2}
 threads_count = ENV.fetch("RAILS_MAX_THREAD") {5}
 threads threads_count, threads_count
 
-bind "tcp://127.0.0.1:3000"
+# bind "tcp://127.0.0.1:3000"
 
 preload_app!
 
