bd0856c838e2a23ef9554e7e689bef454c92a5d4
douglus <douglus@rexy.tech>
2025-04-03 00:36:59 +0700

    fix: allow localhost

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@ workers ENV.fetch("WEB_CONCURRENCY") {2}
 threads_count = ENV.fetch("RAILS_MAX_THREAD") {5}
 threads threads_count, threads_count
 
-bind "unix:///home/staging/bookshop/rails/current/tmp/sockets/puma.sock"
+bind "unix:///home/staging/bookshop/rails/current/tmp/sockets/puma.sock" unless ::Rails.env.development?
 
 preload_app!
 
