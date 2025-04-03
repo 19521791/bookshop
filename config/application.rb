cd4159ce563431bb49b5db0778faaaa29e913aff
douglus <douglus@rexy.tech>
2025-04-03 16:36:34 +0700

    fix: fix cors

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Bookshop
 
     config.middleware.insert_before(0, Rack::Cors) do
       allow do
-        origins '*'
+        origins ['http://localhost:5173', 'https://hub.douglusnguyen.site', 'https://douglusnguyen.site']
         resource '*',
           headers: :any,
           methods: %i[get post patch put delete options],
